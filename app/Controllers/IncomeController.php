ca38f0097479e8fe845d16c00878ec2b7a0c38f1
Ronny Endara <rojaence@gmail.com>
2023-06-14 21:09:38 -0500

    ADD structure of controllers: income and withdrawal

---
+++ app/Controllers/IncomeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controllers;
+
+class IncomeController
+{
+  /**
+   * Muestra una lista del recurso
+  */
+  public function index()
+  {
+
+  }
+
+  /**
+   * Muestra un formulario para crear un nuevo recurso
+  */
+  public function create()
+  {
+
+  }
+
+  /**
+   * Guarda un nuevo recurso en la base de datos
+  */
+  public function store()
+  {
+    
+  }
+
+  /**
+   * Muestra un único recurso
+  */
+  public function show()
+  {
+    
+  }
+
+  /**
+   * Muestra el formulario para ditar un único recurso
+  */
+  public function edit()
+  {
+    
+  }
+
+  /**
+   * Actualiza un recurso en la base de datos
+  */
+  public function update()
+  {
+    
+  }
+
+  /**
+   * Elimina un recurso en la base de datos
+  */
+  public function destroy()
+  {
+    
+  }
+}
